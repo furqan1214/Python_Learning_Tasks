fa6405e5d4cf382c3e0dd609b4d199f8412ccbe9
furqan1214 <furqankhan357159@gmail.com>
2025-07-03 21:25:37 +0500

    Create voting.py

---
+++ voting.py
@@ -0,0 +1,9 @@
+for i in range(1,6):
+  username=input("Enter name: ")
+  age=int(input("Enter age: "))
+  if age>=18:
+    print("you can vote:")
+  else:
+    print("you can not vote:") 
+    
+print("program end")
