b91ceaa8e358a0e6e7b7a226e1861ee18c9fc314
furqan1214 <furqankhan357159@gmail.com>
2025-07-14 20:41:35 +0500

    Create Data-types-from-string-to-dict.py

---
+++ Data-types-from-string-to-dict.py
@@ -0,0 +1,14 @@
+str ="furqan"
+print(str,"str is used for alphabat")
+int =12
+print(int,"int is used for number")
+float =0.123
+print(float,"float is used for point numbers")
+tuple =("furqan","ayan",12)
+print(tuple,"tuple is not change able")
+list =["furqan","ayan",12,13]
+print(list,"list is change able")
+set ={1,2,3,4,"furqan"}
+print(set,"set is not be dublicate")
+dict ={"Name":"Furqan","Age":16,"roll_no":20}
+print(dict,"dict is easy readable")
